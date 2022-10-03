b0bd9959a33f05b2f369fa3d6017d4d349209f22
meyerbw10 <109110828+meyerbw10@users.noreply.github.com>
2022-10-03 19:22:17 -0400

    have pages and nav bar functioning

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React from "react";
-import Header from "./components/Header";
-import Section from "./components/Section";
-import Navbar from "./components/Navbar";
+import PortfolioContainer from "./components/PortfolioContainer";
 
-function App() {
-  return (
-    <div>
-      <Navbar />
-      <Header />
-      <Section />
-    </div>
-  );
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
