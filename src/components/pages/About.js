2f26758e910f6c85f786dfc3d6937c9668c9a586
meyerbw10 <109110828+meyerbw10@users.noreply.github.com>
2022-10-05 18:47:30 -0400

    adding css images and updating links

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div>
-      <h1 class="name">Bradley Meyer</h1>
+    <div class="text-white">
+      <h1 class="name text-black">Bradley Meyer</h1>
       <div>
         <div class="bodyMain">
           <p>
@@ -23,7 +23,7 @@ export default function About() {
           </p>
           <div class="footer">
             <h2>
-              Created by Bradley Meyer
+              Created by <a class='text-white'href='https://www.si.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MTY4MTcwMTMzMDEyMDk2Mzg0/screen_shot_2016-05-28_at_72024_pmpng.png'>B</a>radley Meyer
             </h2>
             <p>
               &copy; 2022 Bradley Meyer
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -11,52 +11,52 @@ import SNapi from '../images/SNAPI.png';
 function Portfolio() {
     return (
         <div>
-            <div class="bodyMain">
+            <div class="bodyMain2 text-white">
                 <div class="allimages">
                     <div class="imagebox">
-                        <a href="https://protected-shelf-14357.herokuapp.com/"><img src={findMyPets} alt="repo-img" class="images"></img></a>
+                        <a href="https://find-my-pets.herokuapp.com/"><img src={findMyPets} alt="repo-img" class="images"></img></a>
                         <div>
                             <h2 class= "imageB">Pet Finder</h2>
                             
                         </div>
                     </div>
                     <div class="imagebox">
-                        <a href="https://skruphold.github.io/Project-1/"><img src={travelGuide} alt="repo-img" class="images"></img></a>
+                        <a href="https://github.com/Galdronis/Travel-Guide"><img src={travelGuide} alt="repo-img" class="images"></img></a>
                         <div>
                             <h2>Travel Guide - Columbus</h2>
                             
                         </div>
                     </div>
                     <div class="imagebox">
-                        <a href="https://shielded-escarpment-21478.herokuapp.com/"><img src={techBlog} alt="repo-img" class="images"></img></a>
+                        <a href="https://github.com/meyerbw10/Tech-Blog"><img src={techBlog} alt="repo-img" class="images"></img></a>
                         <div>
                             <h2>Tech Blog</h2>
                             
                         </div>
                     </div>
                     <div class="imagebox">
-                        <a href="https://grammar-genius.herokuapp.com/"><img src={SNapi} alt="repo-img" class="images"></img></a>
+                        <a href="https://github.com/meyerbw10/Social-Network-API"><img src={SNapi} alt="repo-img" class="images"></img></a>
                         <div>
                             <h2>Social Network API</h2>
                            
                         </div>
                     </div>
                     <div class="imagebox">
-                        <a href="https://workboard-uofm-proj2.herokuapp.com/login"><img src={textEditor} alt="repo-img" class="images"></img></a>
+                        <a href="https://github.com/meyerbw10/Text-Editor"><img src={textEditor} alt="repo-img" class="images"></img></a>
                         <div>
                             <h2>Text Editor</h2>
                             
                         </div>
                     </div>
                     <div class="imagebox">
-                        <a href="https://gist.github.com/JunoNguyen/c5a514447dae2c279ec8c1fe81da3942"><img src={regex} alt="repo-img" class="images"></img></a>
+                        <a href="https://github.com/meyerbw10/Regex-Tutorial"><img src={regex} alt="repo-img" class="images"></img></a>
                         <div>
                             <h2>Regex Tutorial</h2>
                             
                         </div>
                     </div>
                 </div>
-                <div class="footer">
+                <div class="footer text-black">
                     <h2>
                         Created by Bradley Meyer
                     </h2>
